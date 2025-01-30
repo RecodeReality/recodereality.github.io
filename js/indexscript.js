2d3cea50b2e04af7805a9eae6fbda14bdb8241e0
RecodeReality <soulcraft@gmx.com>
2025-01-31 10:14:25 +1030

    Add files via upload

--- js/indexscript.js
+++ js/indexscript.js
@@ -12,6 +12,8 @@ const debugText = document.getElementById('debug');
 // Variables
 // -----
 
+let menuSlide = false;
+
 // -----
 // Script
 // -----
@@ -21,4 +23,34 @@ const showSize = () => {
   debugText.innerText = "Window width: " + width + "px";
 }
 
-showSize();
+//showSize();
+
+const slide = () => {
+  if (!menuSlide) {
+    document.getElementById("headerImage").classList.add("pageOpen");
+    document.getElementById("menu").classList.add("menuSlide");
+    document.getElementById("about").classList.add("openAbout");
+    document.getElementById("library").classList.add("openLibrary");
+    document.getElementById("discord").classList.add("openDiscord");
+    document.getElementById("contact").classList.add("openContact");
+
+    let elements = document.getElementsByClassName("oct-text");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.add("titleText");
+    }
+  } else {
+    document.getElementById("headerImage").classList.remove("pageOpen");
+    document.getElementById("menu").classList.remove("menuSlide");
+    document.getElementById("about").classList.remove("openAbout");
+    document.getElementById("library").classList.remove("openLibrary");
+    document.getElementById("discord").classList.remove("openDiscord");
+    document.getElementById("contact").classList.remove("openContact");
+
+    let elements = document.getElementsByClassName("oct-text");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.remove("titleText");
+    }
+  }
+
+  menuSlide = !menuSlide;
+}
