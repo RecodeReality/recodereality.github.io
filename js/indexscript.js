e30e2aa52a3babf7794c9a950845edba86e89dff
RecodeReality <soulcraft@gmx.com>
2025-01-30 11:26:17 +1030

    Update indexscript.js

--- js/indexscript.js
+++ js/indexscript.js
@@ -7,3 +7,18 @@ Recode Reality Home Page Javascript
 // -----
 
 const debugText = document.getElementById('debug');
+
+// -----
+// Variables
+// -----
+
+// -----
+// Script
+// -----
+
+const showSize = () => {
+  let width = window.innerWidth;
+  debugText.innerText = "Window width: " + width + "px";
+}
+
+showSize();
