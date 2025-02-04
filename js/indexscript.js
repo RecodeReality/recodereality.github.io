d1c0477b06c13312e4f3513d684ac8f39be7ecb0
RecodeReality <soulcraft@gmx.com>
2025-02-05 09:01:26 +1030

    Add files via upload

--- js/indexscript.js
+++ js/indexscript.js
@@ -6,7 +6,6 @@ Recode Reality Home Page Javascript
 // Components
 // -----
 
-//const debugText = document.getElementById('debug');
 
 // -----
 // Variables
@@ -14,6 +13,7 @@ Recode Reality Home Page Javascript
 
 let menuSlide = false;
 let isAboutShown = false;
+let currentPage = 'home';
 
 // -----
 // Script
@@ -21,62 +21,118 @@ let isAboutShown = false;
 
 const showSize = () => {
   let width = window.innerWidth;
-  debugText.innerText = "Window width: " + width + "px";
+  console.log("Window width: " + width + "px");
 }
 
 //showSize();
 
 const slide = () => {
-  if (!menuSlide) {
-    document.getElementById("headerImage").classList.add("pageOpen");
-    document.getElementById("menu").classList.add("menuSlide");
-    document.getElementById("about").classList.add("openAbout");
-    document.getElementById("library").classList.add("openLibrary");
-    document.getElementById("discord").classList.add("openDiscord");
-    document.getElementById("contact").classList.add("openContact");
+  document.getElementById("headerImage").classList.add("pageOpen");
+  document.getElementById("menu").classList.add("menuSlide");
+  document.getElementById("about").classList.add("openAbout");
+  document.getElementById("library").classList.add("openLibrary");
+  document.getElementById("discord").classList.add("openDiscord");
+  document.getElementById("contact").classList.add("openContact");
 
-    let elements = document.getElementsByClassName("oct-text");
-    for (let i = 0; i < elements.length; i++) {
-      elements[i].classList.add("titleText");
-    }
-  } else {
-    document.getElementById("headerImage").classList.remove("pageOpen");
-    document.getElementById("menu").classList.remove("menuSlide");
-    document.getElementById("about").classList.remove("openAbout");
-    document.getElementById("library").classList.remove("openLibrary");
-    document.getElementById("discord").classList.remove("openDiscord");
-    document.getElementById("contact").classList.remove("openContact");
-
-    let elements = document.getElementsByClassName("oct-text");
-    for (let i = 0; i < elements.length; i++) {
-      elements[i].classList.remove("titleText");
-    }
+  let elements = document.getElementsByClassName("oct-text");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add("titleText");
   }
+}
 
-  menuSlide = !menuSlide;
+const slideBack = () => {
+  document.getElementById("headerImage").classList.remove("pageOpen");
+  document.getElementById("menu").classList.remove("menuSlide");
+  document.getElementById("about").classList.remove("openAbout");
+  document.getElementById("library").classList.remove("openLibrary");
+  document.getElementById("discord").classList.remove("openDiscord");
+  document.getElementById("contact").classList.remove("openContact");
+
+  let elements = document.getElementsByClassName("oct-text");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.remove("titleText");
+  }
+}
+
+const hideAbout = () => {
+  // Reverse animation
+  document.getElementById("aboutText").classList.remove("aboutDivText");
+  document.getElementById("aboutText").classList.add("reverseAboutText");
+  document.getElementById("aboutImg").classList.remove("aboutDivImage");
+  document.getElementById("aboutImg").classList.add("reverseAboutImage");
+
+  currentPage = 'home';
+  
+  setTimeout(() => {
+    document.getElementById("aboutPage").style.display = "none";
+  }, 2000);
 }
 
 const showAbout = () => {
-  if (isAboutShown) {
-    // Reverse the animation
-    document.getElementById("aboutText").classList.remove("aboutDivText");
-    document.getElementById("aboutText").classList.add("reverseAboutText");
-    document.getElementById("aboutImg").classList.remove("aboutDivImage");
-    document.getElementById("aboutImg").classList.add("reverseAboutImage");
-    
-    // Set a timeout to hide the #aboutPage element after a short delay
-    setTimeout(() => {
-      document.getElementById("aboutPage").style.display = "none";
-      slide();
-    }, 2000); // Adjust the delay to match the animation duration
-  } else {
-    // Show the about page with animation
+  if (currentPage != 'about') {
     slide();
+    // Play animation
     document.getElementById("aboutPage").style.display = "block";
     document.getElementById("aboutText").classList.remove("reverseAboutText");
     document.getElementById("aboutText").classList.add("aboutDivText");
     document.getElementById("aboutImg").classList.remove("reverseAboutImage");
     document.getElementById("aboutImg").classList.add("aboutDivImage");
+
+    currentPage = 'about';
+
+  } else {
+    hideAbout();
+    setTimeout(() => {
+      slideBack();
+    }, 2000);
   }
-  isAboutShown = !isAboutShown;
+}
+
+const hideContact = () => {
+  // Reverse animation
+
+  currentPage = 'home';
+  
+  setTimeout(() => {
+    document.getElementById("aboutPage").style.display = "none";
+  }, 2000);
+}
+
+const showContact = () => {
+  if (currentPage != 'contact') {
+    slide();
+    // Play animation
+
+    currentPage = 'contact';
+
+  } else {
+    hideContact();
+    setTimeout(() => {
+      slideBack();
+    }, 2000);
+  }
+}
+
+const showPage = (newPage) => {
+  switch(newPage) {
+    case 'home':
+
+      break;
+    case 'about':
+      console.log(newPage);
+      showAbout();
+      break;
+    case 'library':
+      console.log(newPage);
+      break;
+    case 'discord':
+      console.log(newPage);
+      break;
+    case 'contact':
+      console.log(newPage);
+      showContact();
+    break;
+    default:
+      break;
+  }
 }
